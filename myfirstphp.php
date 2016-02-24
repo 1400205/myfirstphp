f62d7e5ff5bffd0e0ec09d3488136e1c8994de23
1400205 <1400205@rgu.ac.uk>
2016-02-24 14:43:42 +0000

    php code

--- myfirstphp.php
+++ myfirstphp.php
@@ -6,6 +6,10 @@
 <p>
     <?php
     echo "Hello World";
+    echo "Hello," . " " . "world" . "!";
+    $myname="Prosper";
+    $myage="111";
+    echo "my name is". $myname ."and my age is" . $myage;
     ?>
 </p>
 </body>
